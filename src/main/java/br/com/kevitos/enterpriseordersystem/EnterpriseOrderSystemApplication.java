a2f75b3027b6c3c3ff73ce95517d70f92ca86455
Kevitos <Kevin.cs07812@gmail.com>
2026-07-29 15:53:18 -0300

    feat: bootstrap Spring Boot project with PostgreSQL and Flyway

---
+++ src/main/java/br/com/kevitos/enterpriseordersystem/EnterpriseOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.kevitos.enterpriseordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kevitos/enterpriseordersystem/EnterpriseOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kevitos.enterpriseordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
