6ab96d699292ba40ae340d26020422ff10d4c240
Kevitos <Kevin.cs07812@gmail.com>
2026-08-17 11:39:10 -0300

    test: add customer not found controller test

---
+++ src/test/java/br/com/kevitos/enterpriseordersystem/controller/CustomerControllerTest.java
@@ -0,0 +1,39 @@
+package br.com.kevitos.enterpriseordersystem.controller;
+
+import br.com.kevitos.enterpriseordersystem.service.CustomerService;
+
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import br.com.kevitos.enterpriseordersystem.exception.CustomerNotFoundException;
+
+@WebMvcTest(CustomerController.class)
+class CustomerControllerTest {
+
+    @MockitoBean
+    private CustomerService customerService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturn404WhenCustomerDoesNotExist() throws Exception {
+
+        doThrow(new CustomerNotFoundException("Cliente não encontrado para o ID: 999"))
+                .when(customerService)
+                .getCustomerById(999L);
+
+        mockMvc.perform(get("/customers/999"))
+        .andExpect(status().isNotFound())
+        .andExpect(jsonPath("$.status").value(404))
+        .andExpect(jsonPath("$.message")
+                .value("Cliente não encontrado para o ID: 999"));
+    }
+}
